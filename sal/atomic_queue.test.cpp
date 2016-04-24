064c1198f4cdcc720d4ca4ad136a20434b79cc29
Sven Suursoho <sven+sal@alt.ee>
2016-04-24 13:21:21 +0300

    atomic_queue/test: parametrise UsePolicy instead of queue type

--- sal/atomic_queue.test.cpp
+++ sal/atomic_queue.test.cpp
@@ -18,6 +18,9 @@ TYPED_TEST_CASE_P(test);
 struct foo
 {
   sal::atomic_queue_hook<foo> hook;
+
+  template <typename UsePolicy>
+  using queue = sal::atomic_queue<foo, &foo::hook, UsePolicy>;
 };
 
 
@@ -30,23 +33,21 @@ constexpr bool is_lock_free () noexcept
 
 TYPED_TEST_P(test, lock_free)
 {
-  TypeParam q;
-  EXPECT_EQ(is_lock_free<typename TypeParam::use_policy>(),
-    q.is_lock_free()
-  );
+  foo::queue<TypeParam> q;
+  EXPECT_EQ(is_lock_free<TypeParam>(), q.is_lock_free());
 }
 
 
 TYPED_TEST_P(test, ctor)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
   ASSERT_EQ(nullptr, q.try_pop());
 }
 
 
 TYPED_TEST_P(test, move_ctor_empty)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
   ASSERT_EQ(nullptr, q.try_pop());
 
   auto q1 = std::move(q);
@@ -56,7 +57,7 @@ TYPED_TEST_P(test, move_ctor_empty)
 
 TYPED_TEST_P(test, move_ctor_non_empty)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
 
   foo f;
   q.push(&f);
@@ -69,10 +70,10 @@ TYPED_TEST_P(test, move_ctor_non_empty)
 
 TYPED_TEST_P(test, move_assign_empty)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
   ASSERT_EQ(nullptr, q.try_pop());
 
-  TypeParam q1;
+  foo::queue<TypeParam> q1;
   ASSERT_EQ(nullptr, q1.try_pop());
 
   q1 = std::move(q);
@@ -82,12 +83,12 @@ TYPED_TEST_P(test, move_assign_empty)
 
 TYPED_TEST_P(test, move_assign_non_empty)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
 
   foo f;
   q.push(&f);
 
-  TypeParam q1;
+  foo::queue<TypeParam> q1;
   q1 = std::move(q);
   ASSERT_EQ(&f, q1.try_pop());
   ASSERT_EQ(nullptr, q1.try_pop());
@@ -96,7 +97,7 @@ TYPED_TEST_P(test, move_assign_non_empty)
 
 TYPED_TEST_P(test, single_push_pop)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
 
   foo f;
   q.push(&f);
@@ -108,7 +109,7 @@ TYPED_TEST_P(test, single_push_pop)
 
 TYPED_TEST_P(test, multiple_push_pop)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
 
   foo f1, f2, f3;
   q.push(&f1);
@@ -125,7 +126,7 @@ TYPED_TEST_P(test, multiple_push_pop)
 
 TYPED_TEST_P(test, interleaved_push_pop)
 {
-  TypeParam q;
+  foo::queue<TypeParam> q;
 
   foo f1, f2;
   q.push(&f1);
@@ -162,14 +163,7 @@ REGISTER_TYPED_TEST_CASE_P(test,
 );
 
 
-using types = testing::Types<
-  sal::atomic_queue<foo, &foo::hook, sal::spsc>,
-  sal::atomic_queue<foo, &foo::hook, sal::mpsc>,
-  sal::atomic_queue<foo, &foo::hook, sal::spmc>,
-  sal::atomic_queue<foo, &foo::hook, sal::mpmc>
->;
-
-
+using types = testing::Types<sal::spsc, sal::mpsc, sal::spmc, sal::mpmc>;
 INSTANTIATE_TYPED_TEST_CASE_P(atomic_queue, test, types);
 
 
