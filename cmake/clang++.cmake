2574ede8df3932723cda54a99c68fc10f4c6fa1a
Sven Suursoho <sven+sal@alt.ee>
2016-05-05 17:39:20 +0300

    linking: fix -latomic on linux/clang

--- cmake/clang++.cmake
+++ cmake/clang++.cmake
@@ -4,3 +4,8 @@
 
 # Compatible with g++
 include(cmake/g++.cmake)
+
+# Dependencies required for linking executables
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+  list(APPEND SAL_DEP_LIBS atomic)
+endif()
