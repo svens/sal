dcb5b8b7a02f428f0c6b71c12662d12c24981c5b
Sven Suursoho <sven+sal@alt.ee>
2016-05-06 18:01:26 +0000

    this_thread: get_id for Linux

--- sal/thread.cpp
+++ sal/thread.cpp
@@ -3,10 +3,8 @@
 #if __sal_os_darwin
   #include <pthread.h>
 #elif __sal_os_linux
-  /*
   #include <unistd.h>
   #include <sys/syscall.h>
-  */
 #elif __sal_os_windows
   #include <Windows.h>
 #else
@@ -26,9 +24,7 @@ thread_id make_id () noexcept
 #if __sal_os_darwin
   return pthread_mach_thread_np(pthread_self());
 #elif __sal_os_linux
-  /*
   return syscall(SYS_gettid);
-  */
 #elif __sal_os_windows
   return GetCurrentThreadId();
 #endif
