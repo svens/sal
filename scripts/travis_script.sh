b872f7ed4cf707270fdc1aa3328ee0449ecee384
Sven Suursoho <sven+sal@alt.ee>
2016-09-10 19:07:14 +0300

    Coverity: add coverity_scan addon to linux-gcc

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,6 +1,9 @@
-cmake . \
-  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
-  -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
-  -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-cmake --build .
-cmake --build . --target ${TEST_TARGET} -- ARGS=--output-on-failure
+# Coverity builds are handled by addons.coverity_scan
+if test "${BUILD_TYPE}" != "Coverity"; then
+  cmake . \
+    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
+    -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+  cmake --build .
+  cmake --build . --target ${TEST_TARGET} -- ARGS=--output-on-failure
+fi
