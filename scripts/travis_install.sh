c853425140f6cf3f717310e38d07706d7006c922
Sven Suursoho <sven+sal@alt.ee>
2016-09-10 15:36:33 +0300

    travis: explicit build matrix

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,10 +1,8 @@
 case "${TRAVIS_OS_NAME}-${CC}" in
   linux-clang)
-    export CC=clang-3.8 CXX=clang++-3.8
     ;;
 
   linux-gcc)
-    export CC=gcc-6 CXX=g++-6 GCOV=gcov-6
     if test "${TEST_TARGET}" = "gen-cov"; then
       curl -sO http://ftp.debian.org/debian/pool/main/l/lcov/lcov_1.12.orig.tar.gz
       gunzip -c lcov_1.12.orig.tar.gz | tar xvf -
@@ -13,13 +11,11 @@ case "${TRAVIS_OS_NAME}-${CC}" in
     ;;
 
   osx-clang)
-    export CC=clang CXX=clang++
     brew update
     brew install cmake || true
     ;;
 
   osx-gcc)
-    export CC=gcc-6 CXX=g++-6
     brew update
     brew install cmake gcc || true
     ;;
