eabbd6fb0bd7fe2f4c4327680a1bb7f767139b04
Sven Suursoho <sven+sal@alt.ee>
2016-10-02 17:28:29 +0300

    sal::throw_error

--- sal/error.hpp
+++ sal/error.hpp
@@ -9,19 +9,32 @@
 #include <sal/str.hpp>
 #include <stdexcept>
 #include <system_error>
+#include <type_traits>
 
 
 namespace sal {
 __sal_begin
 
 
+template <typename Error, typename... Args>
+inline void throw_error [[noreturn]] (Args &&...args)
+{
+  static_assert(std::is_base_of<std::exception, Error>::value,
+    "exceptions should be inherited from std::exception"
+  );
+  str_t<1 + 128*sizeof...(Args)> what;
+  print(what, std::forward<Args>(args)...);
+  throw Error(what.get());
+}
+
+
 /// Throw std::logic_error with message composed from list of \a args.
 template <typename... Args>
 inline void throw_logic_error [[noreturn]] (Args &&...args)
 {
-  str_t<128*sizeof...(Args)> what;
+  str_t<1 + 128*sizeof...(Args)> what;
   print(what, std::forward<Args>(args)...);
-  throw std::logic_error(what.get()); // LCOV_EXCL_BR_LINE
+  throw std::logic_error(what.get());
 }
 
 
@@ -29,9 +42,9 @@ inline void throw_logic_error [[noreturn]] (Args &&...args)
 template <typename... Args>
 inline void throw_runtime_error [[noreturn]] (Args &&...args)
 {
-  str_t<128*sizeof...(Args)> what;
+  str_t<1 + 128*sizeof...(Args)> what;
   print(what, std::forward<Args>(args)...);
-  throw std::runtime_error(what.get()); // LCOV_EXCL_BR_LINE
+  throw std::runtime_error(what.get());
 }
 
 
@@ -40,9 +53,9 @@ template <typename... Args>
 inline void throw_system_error [[noreturn]] (const std::error_code &error,
   Args &&...args)
 {
-  str_t<128*sizeof...(Args)> what;
+  str_t<1 + 128*sizeof...(Args)> what;
   print(what, std::forward<Args>(args)...);
-  throw std::system_error(error, what.get()); // LCOV_EXCL_BR_LINE
+  throw std::system_error(error, what.get());
 }
 
 
