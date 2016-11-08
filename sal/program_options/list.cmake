12844360b4f624d336ca102176f945a761434e1c
Sven Suursoho <spam@alt.ee>
2016-11-08 15:17:34 +0200

    program_options: drop half-baked YAML config reader

--- sal/program_options/list.cmake
+++ sal/program_options/list.cmake
@@ -10,8 +10,6 @@ list(APPEND sal_sources
   sal/program_options/command_line.hpp
   sal/program_options/command_line.cpp
   sal/program_options/error.hpp
-  sal/program_options/yaml_reader.hpp
-  sal/program_options/yaml_reader.cpp
   sal/program_options/option_set.hpp
   sal/program_options/option_set.cpp
 )
@@ -21,6 +19,5 @@ list(APPEND sal_sources
 list(APPEND sal_unittests
   sal/program_options/common.test.hpp
   sal/program_options/command_line.test.cpp
-  sal/program_options/yaml_reader.test.cpp
   sal/program_options/option_set.test.cpp
 )
