da1bc3af38135d07f8d12bf717482e860e19111b
Sven Suursoho <sven@alt.ee>
2017-05-19 21:03:08 +0300

    Travis-CI: export CC, CXX, GCOV env variables during install phase

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,13 +1,13 @@
 case "${TRAVIS_OS_NAME}-${CC}" in
   linux-clang)
-    CC=clang-3.8
-    CXX=clang++-3.8
+    export CC=clang-3.8
+    export CXX=clang++-3.8
     ;;
 
   linux-gcc)
-    CC=gcc-6
-    CXX=g++-6
-    GCOV=gcov-6
+    export CC=gcc-6
+    export CXX=g++-6
+    export GCOV=gcov-6
     if test "${BUILD_TYPE}" = "Coverage"; then
       curl -sO http://ftp.debian.org/debian/pool/main/l/lcov/lcov_1.13.orig.tar.gz
       gunzip -c lcov_1.13.orig.tar.gz | tar xvf -
@@ -17,15 +17,15 @@ case "${TRAVIS_OS_NAME}-${CC}" in
     ;;
 
   osx-clang)
-    CC=clang
-    CXX=clang++
+    export CC=clang
+    export CXX=clang++
     brew update
     brew install cmake || true
     ;;
 
   osx-gcc)
-    CC=gcc-7
-    CXX=g++-7
+    export CC=gcc-7
+    export CXX=g++-7
     brew update
     brew install cmake gcc || true
     ;;
--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,12 +1,8 @@
 if test "${BUILD_TYPE}" = "Coverity"; then
-  cmake . \
-    -DSAL_UNITTESTS=no \
-    -DSAL_BENCH=no
+  cmake . -DSAL_UNITTESTS=no -DSAL_BENCH=no
   bash <(curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh)
 elif test "${BUILD_TYPE}" = "Coverage"; then
-  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-    -DSAL_UNITTESTS=yes \
-    -DSAL_BENCH=no
+  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
   cmake --build .
   cmake --build . --target gen-cov -- ARGS=--output-on-failure
 else
