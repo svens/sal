0f828782a347afe79eeff1ec092ea41fbb99f132
Sven Suursoho <sven@alt.ee>
2017-05-19 20:19:36 +0300

    Travis-CI: override CC & CXX directly instead of cmake variables

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,22 +1,16 @@
 if test "${BUILD_TYPE}" = "Coverity"; then
   cmake . \
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
     -DSAL_UNITTESTS=no \
     -DSAL_BENCH=no
   bash <(curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh)
 elif test "${BUILD_TYPE}" = "Coverage"; then
   cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
     -DSAL_UNITTESTS=yes \
     -DSAL_BENCH=no
   cmake --build .
   cmake --build . --target gen-cov -- ARGS=--output-on-failure
 else
-  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
   cmake --build .
   cmake --build . --target test -- ARGS=--output-on-failure
 fi
