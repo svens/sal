d5349fab2945dab7d4d5fb18b115c6ec5cdf7b62
Sven Suursoho <sven@alt.ee>
2017-09-29 11:58:42 +0300

    Move gtest & doxygen discovery to own cmake modules

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,14 @@
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+  configure_file(
+    ${PROJECT_SOURCE_DIR}/cmake/Doxyfile.in
+    ${CMAKE_BINARY_DIR}/Doxyfile
+  )
+  add_custom_target(gen-doc
+    ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMENT "Generating documentation"
+  )
+else()
+  message(WARNING "Doxygen not found, skipping documentation generation")
+endif()
---
+++ cmake/gtest.cmake
@@ -0,0 +1,33 @@
+find_package(Threads)
+find_package(GTest QUIET)
+if(NOT GTEST_FOUND)
+  message(STATUS "Building own gtest")
+
+  # download and build
+  include(ExternalProject)
+  ExternalProject_Add(gtest
+    URL https://github.com/google/googletest/archive/release-1.7.0.zip
+    URL_HASH SHA256=b58cb7547a28b2c718d1e38aee18a3659c9e3ff52440297e965f5edffe34b6d0
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest-1.7.0
+    CMAKE_ARGS
+      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+  )
+
+  # gtest headers
+  ExternalProject_Get_Property(gtest source_dir)
+  set(GTEST_INCLUDE_DIRS ${source_dir}/include)
+
+  # gtest libs
+  ExternalProject_Get_Property(gtest binary_dir)
+  link_directories(${binary_dir})
+  set(GTEST_BOTH_LIBRARIES
+    ${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
+    ${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}
+  )
+endif()
