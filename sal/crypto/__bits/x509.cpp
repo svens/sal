4f983d57ce2c57819d70e67ee7487900f5eb478e
Sven Suursoho <sven@alt.ee>
2017-11-04 19:25:33 +0200

    crypto/pem_to_der: use precalculated lookup table for base64 decode

--- sal/crypto/__bits/x509.cpp
+++ sal/crypto/__bits/x509.cpp
@@ -11,49 +11,44 @@ namespace crypto { namespace __bits {
 
 namespace {
 
-struct base64_t
-{
-  static constexpr uint8_t encode[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz"
-    "0123456789"
-    "+/";
-
-  uint8_t decode[256];
-
-  constexpr base64_t ()
-    : decode{}
-  {
-    for (auto &i: decode)
-    {
-      i = 0xff;
-    }
-    for (int i = 0;  i != 64;  ++i)
-    {
-      decode[static_cast<size_t>(encode[i])] = static_cast<uint8_t>(i);
-    }
-  }
-};
 
 uint8_t *base64_decode (const uint8_t *first, const uint8_t *last, uint8_t *d)
   noexcept
 {
-  static constexpr base64_t base64;
+  static constexpr const unsigned char lookup[] =
+    // _0  _1  _2  _3  _4  _5  _6  _7  _8  _9  _a  _b  _c  _d  _e  _f
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // 0_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // 1_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x3e\xff\xff\xff\x3f" // 2_
+    "\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\xff\xff\xff\xff\xff\xff" // 3_
+    "\xff\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e" // 4_
+    "\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\xff\xff\xff\xff\xff" // 5_
+    "\xff\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28" // 6_
+    "\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\xff\xff\xff\xff\xff" // 7_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // 8_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // 9_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // a_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // b_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // c_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // d_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // e_
+    "\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff" // f_
+  ;
 
   for (int32_t val = 0, valb = -8;  first != last;  ++first)
   {
-    if (std::isspace(*first))
+    if (lookup[*first] != 0xff)
     {
-      continue;
-    }
-    else if (base64.decode[*first] != 0xff)
-    {
-      val = (val << 6) | base64.decode[*first];
+      val = (val << 6) | lookup[*first];
       if ((valb += 6) >= 0)
       {
         *d++ = static_cast<uint8_t>((val >> valb) & 0xff);
         valb -= 8;
       }
+    }
+    else if (std::isspace(*first))
+    {
+      continue;
     }
     else if ((last - first == 1 && first[0] == '=')
       || (last - first == 2 && first[0] == '=' && first[1] == '='))
@@ -68,6 +63,7 @@ uint8_t *base64_decode (const uint8_t *first, const uint8_t *last, uint8_t *d)
 
   return d;
 }
+
 
 } // namepsace
 
@@ -121,7 +117,7 @@ uint8_t *pem_to_der (const uint8_t *pem_first, const uint8_t *pem_last,
   auto max_decoded_size = (last - first) / 4 * 3;
   if (der_last - der_first >= max_decoded_size)
   {
-    // decode (can't use sal::encode<base64> because of whitepsaces)
+    // decode (can't use sal::encode<base64> because of whitespaces)
     return base64_decode(first, last, der_first);
   }
 
