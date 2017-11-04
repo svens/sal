b8649edec68814472b9f3695037501d91fdb844f
Sven Suursoho <sven@alt.ee>
2017-11-04 13:10:48 +0200

    Make fnv_1a truly constexpr

--- sal/hash.hpp
+++ sal/hash.hpp
@@ -12,20 +12,12 @@
 __sal_begin
 
 
-#if defined(_MSC_VER)
-  #define __sal_hash_constexpr inline
-#else
-  #define __sal_hash_constexpr constexpr
-#endif
-
-
 /**
  * Hash two 64bit values into single 64bit value. This implementation is
  * copied from Google's CityHash. See ThirdPartySources.txt for copyright
  * notices.
  */
-__sal_hash_constexpr
-uint64_t hash_128_to_64 (uint64_t h, uint64_t l) noexcept
+constexpr uint64_t hash_128_to_64 (uint64_t h, uint64_t l) noexcept
 {
   constexpr uint64_t mul = 0x9ddfea08eb382d69ULL;
   uint64_t a = (l ^ h) * mul;
@@ -44,15 +36,13 @@ uint64_t hash_128_to_64 (uint64_t h, uint64_t l) noexcept
  *
  * See ThirdPartySources.txt for copyright notices.
  */
-__sal_hash_constexpr
-uint64_t fnv_1a_64 (const void *first, const void *last,
-  uint64_t h = 0xcbf29ce484222325ULL) noexcept
+template <typename It>
+constexpr uint64_t fnv_1a_64 (It first, It last, uint64_t h = 0xcbf29ce484222325ULL)
+  noexcept
 {
-  auto p = static_cast<const int8_t *>(first);
-  auto e = static_cast<const int8_t *>(last);
-  while (p != e)
+  while (first != last)
   {
-    h ^= *p++;
+    h ^= *first++;
     h += (h << 1) + (h << 4) + (h << 5) + (h << 7) + (h << 8) + (h << 40);
   }
   return h;
