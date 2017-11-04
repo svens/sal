fef3467775b71e956e3b1cee8874c33ee418f0df
Sven Suursoho <sven@alt.ee>
2017-11-04 13:12:07 +0200

    type_id: remove type_id constexpr attribute, add truly constexpr type_v

--- sal/type_id.hpp
+++ sal/type_id.hpp
@@ -16,6 +16,33 @@ namespace __bits {
 template <typename T>
 const T *unique_address_for{};
 
+
+template <typename T>
+struct type_t
+{
+  static constexpr uint64_t id () noexcept
+  {
+#if defined(__GNUC__)
+    return fnv_1a_32(__PRETTY_FUNCTION__);
+#elif defined(_MSC_VER)
+    return fnv_1a_32(__FUNCSIG__);
+#endif
+  }
+
+  template <size_t N>
+  static constexpr uint64_t fnv_1a_32 (const char (&buf)[N]) noexcept
+  {
+    uint64_t h = 0x811c9dc5;
+    for (auto x = buf;  x != buf + N - 1;  ++x)
+    {
+      h ^= *x;
+      h += static_cast<uint32_t>((h << 1) + (h << 4) + (h << 7) + (h << 8) + (h << 24));
+    }
+    return h;
+  }
+};
+
+
 } // namespace __bits
 
 
@@ -23,7 +50,7 @@ const T *unique_address_for{};
  * Return unique id for T amongst all types.
  */
 template <typename T>
-inline constexpr uintptr_t type_id () noexcept
+inline uintptr_t type_id () noexcept
 {
   return reinterpret_cast<uintptr_t>(&__bits::unique_address_for<T>);
 }
@@ -33,10 +60,19 @@ inline constexpr uintptr_t type_id () noexcept
  * \copydoc type_id()
  */
 template <typename T>
-inline constexpr uintptr_t type_id (T) noexcept
+inline uintptr_t type_id (T) noexcept
 {
   return reinterpret_cast<uintptr_t>(&__bits::unique_address_for<T>);
 }
+
+
+/**
+ * Return pseudorandom type id for T. Although highly unlikely, it's value
+ * might clash with value of some unrelated type. It has advantage compared to
+ * type_id() of being constexpr.
+ */
+template <typename T>
+constexpr const auto type_v = __bits::type_t<T>::id();
 
 
 __sal_end
--- sal/type_id.unit_a.test.cpp
+++ sal/type_id.unit_a.test.cpp
@@ -2,7 +2,13 @@
 #include <sal/common.test.hpp>
 
 
-uintptr_t type_in_unit_a () noexcept
+uintptr_t type_id_in_unit_a () noexcept
 {
   return sal::type_id<sal_test::fixture>();
+}
+
+
+uintptr_t type_v_in_unit_a () noexcept
+{
+  return sal::type_v<sal_test::fixture>;
 }
