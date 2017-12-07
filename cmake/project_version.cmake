1f5c947e71ec90697599b667ca653018da0fda0d
Sven Suursoho <sven@alt.ee>
2017-12-07 18:40:33 +0200

    Simplify cmake/project_version
    [skip ci]

--- cmake/project_version.cmake
+++ cmake/project_version.cmake
@@ -1,32 +1,18 @@
 find_package(Git)
 if(GIT_EXECUTABLE)
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --match "v[0-9]*.[0-9]*.[0-9]*"
-    RESULT_VARIABLE exit_code
+    COMMAND ${GIT_EXECUTABLE} describe --match "v[0-9]*.[0-9]*.[0-9]*" --always --tags --dirty
     OUTPUT_VARIABLE PROJECT_VERSION
     ERROR_QUIET
   )
-  if(${exit_code} EQUAL 0)
-    # v{VERSION}-{N}-g{HASH} -> {VERSION}-{HASH}
-    string(REGEX REPLACE "^v" "" PROJECT_VERSION ${PROJECT_VERSION})
-    string(REGEX
-      REPLACE "^([^-]*)-[0-9]+-g([0-9a-f]*)" "\\1-\\2"
-      PROJECT_VERSION
-      ${PROJECT_VERSION}
-    )
-  else()
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-      RESULT_VARIABLE exit_code
-      OUTPUT_VARIABLE PROJECT_VERSION
-      ERROR_QUIET
-    )
-    if(${exit_code})
-      set(PROJECT_VERSION "0.0.0")
-    endif()
-  endif()
+
+  # v{VERSION}-{N}-g{HASH} -> {VERSION}-{HASH}
+  string(STRIP ${PROJECT_VERSION} PROJECT_VERSION)
+  string(REGEX
+    REPLACE "^v?([0-9]*.[0-9]*.[0-9]*)-[0-9]+-g([0-9a-f]*)" "\\1-\\2"
+    PROJECT_VERSION
+    ${PROJECT_VERSION}
+  )
 else()
   set(PROJECT_VERSION "0.0.0")
 endif()
-
-string(STRIP ${PROJECT_VERSION} PROJECT_VERSION)
