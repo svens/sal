12e0b1f904a19abad958832cc33819c44e6555f0
Sven Suursoho <sven@alt.ee>
2017-12-09 11:38:47 +0200

    Rename tps/googletest -> tps/gtest

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,13 +1,16 @@
-find_package(Git)
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} submodule update --init tps/googletest
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-)
+set(GTEST_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/tps/gtest/include")
 
-add_subdirectory(tps/googletest)
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}/tps/googletest/include)
+if(NOT EXISTS ${GTEST_INCLUDE_DIR}/gtest/gtest.h)
+  find_package(Git)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} submodule update --init tps/gtest
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+  )
+endif()
 
-# relax compiler checks for tests, intentionally creating unappy paths
+include_directories(${GTEST_INCLUDE_DIR})
+add_subdirectory(tps/gtest)
+
 if(CMAKE_COMPILER_IS_GNUCXX OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
   target_compile_options(gtest
     PUBLIC -Wno-effc++ -Wno-extra
