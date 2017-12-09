289a127b14717c2f130345c0a8ad7787fe45c5ae
Sven Suursoho <sven@alt.ee>
2017-12-09 11:59:53 +0200

    Use project name prefix for options

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,12 +1,12 @@
 if test "${BUILD_TYPE}" = "Coverity"; then
-  cmake . -DUNITTESTS=no -DDOCS=no -DBENCH=no
+  cmake . -Dsal_unittests=no -Dsal_docs=no -Dsal_bench=no
   bash <(curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh)
 elif test "${BUILD_TYPE}" = "Coverage"; then
-  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DDOCS=no -DBENCH=no
+  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -Dsal_docs=no -Dsal_bench=no
   cmake --build .
   travis_wait cmake --build . --target gen-cov -- ARGS=--output-on-failure
 else
-  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DDOCS=no -DBENCH=no
+  cmake . -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -Dsal_docs=no -Dsal_bench=no
   cmake --build .
   #cmake --build . --target test -- ARGS=--output-on-failure
   ./unittests --gtest_filter="*:-net_socket/?.connect*_v6:net_ip/*/1"
