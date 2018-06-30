da57d2cc34b1ec144d67dac0c49ad5f6bdd71f52
Sven Suursoho <sven@alt.ee>
2018-06-30 10:55:59 +0300

    Add MSVC warnings (off by default at /W4)

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -2,11 +2,19 @@
 # MSVC options
 #
 
-set(CMAKE_CXX_FLAGS "/nologo /std:c++latest /W4 /WX /EHsc")
+set(CMAKE_CXX_FLAGS "/nologo /std:c++latest /EHsc /W4 /WX")
 set(CMAKE_CXX_FLAGS_DEBUG "/D_DEBUG /Zi /Od /RTC1 /sdl /MTd")
 set(CMAKE_CXX_FLAGS_RELEASE "/DNDEBUG /Ox /MT")
 
 add_definitions(
   /D _SBCS
   /D WIN32_LEAN_AND_MEAN
+)
+
+# https://docs.microsoft.com/en-us/cpp/preprocessor/compiler-warnings-that-are-off-by-default
+string(CONCAT CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}"
+  " /w34265"
+  " /w34777"
+  " /w34946"
+  " /w35038"
 )
